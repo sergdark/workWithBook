3347432e1abd55177cb4d1bd4c3d215f356f9634
sergdark <sergdarkjob@gmail.com>
2019-04-11 14:46:51 +0300

    20194011_00

---
+++ 1/PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController:Controller{
+        private DataContext context;
+
+        public HomeController(DataContext ctx) => context = ctx;
+    }
+}
