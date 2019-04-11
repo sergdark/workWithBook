4c75d588ff49e5295a0a745a1b0c766ac6efbb55
sergdark <sergdarkjob@gmail.com>
2019-04-11 16:41:00 +0300

    20194011_01

--- 1/PartyInvites/PartyInvites/Controllers/HomeController.cs
+++ 1/PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -1,15 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PartyInvites.Models;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace PartyInvites.Controllers
 {
-    public class HomeController:Controller{
+    public class HomeController : Controller
+    {
         private DataContext context;
-
         public HomeController(DataContext ctx) => context = ctx;
+        public IActionResult Index() => View();
+        public IActionResult Respond() => View();
+        [HttpPost]
+        public IActionResult Respond(GuestResponse response)
+        {
+            context.Responses.Add(response);
+            context.SaveChanges();
+            return RedirectToAction(nameof(Thanks),
+            new { Name = response.Name, WillAttend = response.WillAttend });
+        }
+        public IActionResult Thanks(GuestResponse response) => View(response);
+        public IActionResult ListResponses() => View(context.Responses.OrderByDescending(r => r.WillAttend));
     }
+
 }
--- 1/PartyInvites/PartyInvites/Models/GuestResponse.cs
+++ 1/PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace PartyInvites.Models
+﻿namespace PartyInvites.Models
 {
     public class GuestResponse
     {
+        public long Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public string Photo { get; set; }
-        public bool? willAttend { get; set; }
+        public string Phone { get; set; }
+        public bool WillAttend { get; set; }
     }
 }
