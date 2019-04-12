a352dc7156fe502fac32c46cc3aa35a4dd4f8ab5
sergdark <sergdarkjob@gmail.com>
2019-04-12 16:33:52 +0300

    20194011_03

---
+++ 2/myPartyInvites/myPartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using myPartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myPartyInvites.Controllers
+{
+    public class HomeController: Controller
+    {
+        private DataContext context;
+
+        public HomeController(DataContext ctx) => context = ctx;
+
+        public IActionResult Index() => View();
+
+        public IActionResult Respond() => View();
+
+        [HttpPost]
+        public IActionResult Respond(GuestResponse response)
+        {
+            context.Responses.Add(response);
+            context.SaveChanges();
+            return RedirectToAction(nameof(Thanks),
+                new { Name = response.Name, WillAttend = response.WillAttend });
+        }
+
+        public IActionResult Thanks(GuestResponse response) => View(response);
+
+        public IActionResult ListResponses() =>
+            View(context.Responses.OrderByDescending(r => r.WillAttend));
+    }
+}
